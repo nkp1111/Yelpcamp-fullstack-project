174cde88c69f6e84719b1e9916fcc3e7ae4dee7c
nkp1111 <neerajp4321@gmail.com>
2023-02-13 16:39:00 +0530

    Fix: last return page before login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    req.session.returnTo = req.originalUrl
     req.flash("error", "You must be Signed in first!")
     return res.redirect("/login")
   }
