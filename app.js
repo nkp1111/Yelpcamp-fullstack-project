2f403dce2f1fcc6499e005a8620470ddbed0bdb6
nkp1111 <neerajp4321@gmail.com>
2023-02-16 21:25:11 +0530

    Chore: add port through env file

--- app.js
+++ app.js
@@ -170,6 +170,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err })
 })
 
-app.listen("3000", () => {
+app.listen(process.env["PORT"] || 3000, () => {
   console.log("App is listening on port 3000");
 })
